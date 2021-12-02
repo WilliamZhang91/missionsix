1265b71337bfd28c8df2679b268de32a9b4125f3
william <williamjzh75@gmail.com>
2021-12-03 02:46:11 +1300

    heroku deploy

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 const mongoPractice = require("./mongoose")
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
 app.use(cors());
+const path = require('path')
 
 
 app.use(bodyParser.json());
@@ -14,4 +17,15 @@ app.get('/products', mongoPractice.getProducts);
 
 app.post('/contacts', mongoPractice.createContact);
 
-app.listen(4000);
+if (process.env.NODE_ENV === "production") {  
+    app.use(express.static(path.join(__dirname, "../frontend/build")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.send("Api running")
+    })
+}
+
+app.listen(port);
