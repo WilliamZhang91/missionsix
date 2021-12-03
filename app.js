87c59c77cb141a809263bedec4bb63f9757fa961
william <williamjzh75@gmail.com>
2021-12-03 14:32:42 +1300

    minor changes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const mongoPractice = require("./mongoose");
 const bodyParser = require('body-parser');
-const mongoPractice = require("./mongoose")
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 app.use(cors());
-const path = require('path')
+const path = require('path');
 
 
 app.use(bodyParser.json());
@@ -20,7 +20,7 @@ app.post('/contacts', mongoPractice.createContact);
 if (process.env.NODE_ENV === "production") {  
     app.use(express.static(path.join(__dirname, "frontend/build")))
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
     });
 } else {
     app.get("/", (req, res) => {
